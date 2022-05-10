0ad9033361901f47f343797cb231b555fe771faf
osaro-hero <osaroosagie61@yahoo.com>
2022-05-10 09:02:33 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a4d26645-6cb5-4703-ba84-58f6b4e55357';
+export const MODAL_CLOSE_SEC = 2.5;
